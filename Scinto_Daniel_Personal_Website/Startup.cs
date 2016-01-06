643200c60f85b76208a8bd1b24dd3a74d8d163ea
dpscinto <dpscinto@gmail.com>
2016-01-06 15:33:36 -0500

    First Commit
    Updated Twitter Bootstrap Template

---
+++ Scinto_Daniel_Personal_Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scinto_Daniel_Personal_Website.Startup))]
+namespace Scinto_Daniel_Personal_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
